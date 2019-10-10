be8f6321160514855b6d27cf1b8a5e93c788853f
qll <qll19930105@163.com>
2019-10-10 23:19:17 +0800

    add index_caousel by swiper

---
+++ app/src/components/IndexCaousel.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+           <swiper-slide v-for="(item,index) of imgs" :key="index">
+              <img :src="item">
+              <p>{{item}}</p>
+           </swiper-slide>
+           <div class="swiper-pagination"  slot="pagination"></div>
+           <!-- <div class="swiper-button-prev" slot="button-prev"></div> -->
+           <!-- <div class="swiper-button-next" slot="button-next"></div> -->
+       </swiper>
+    </div>
+      
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            imgs:["../../public/images/banner/banner01.png","/images/banner/banner02.png"],
+           swiperOption: {
+              pagination: {
+                el: '.swiper-pagination'
+              },
+              autoplay:true
+            }
+        }
+    },
+    created(){
+        this.axios.get("http://127.0.0.1:3000/index/banner")
+        .then(res=>{
+            // this.imgs=res.data;
+            // console.log(this.imgs)
+        })
+    }
+}
+</script>
+
+<style scoped>
+  /* .wrapper{
+      width:500px;
+      height:500px;
+  } */
+  img{
+      width:100%;
+      height:500px;
+  }
+          
+</style>
--- server/app.js
+++ server/app.js
@@ -6,10 +6,10 @@ const pool = require("./pool.js");
 const mysql = require("mysql");
 
 var app = express();
-app.listen(8080);
+app.listen(3000);
 
 app.use(cors({
-    origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5050"],
+    origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5050","http://127.0.0.1:8081", "http://127.0.0.1:8080"],
     credentials: true
 }))
 app.use(session({
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 //获取首页轮播图列表 
-app.get("/", (req, res) => {
+app.get("/index/banner", (req, res) => {
         var sql = "SELECT * FROM mcake_pc_banner";
         pool.query(sql, [], (err, result) => {
             if (err) throw err;
